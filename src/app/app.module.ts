853e9ee0e25575d428937d8c0065066947690074
qpalzm88 <qpalzm@list.ru>
2020-09-12 13:22:43 +0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppDirective } from './app.directive';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRippleModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatRippleModule,
+    MatInputModule,
   ],
+  exports: [
+    BrowserModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatRippleModule,
+    MatInputModule,],
+
   providers: [],
   bootstrap: [AppComponent]
 })
